3db3cab46f513ba9ee10379e9167a763d9040bc3
Parsa <Mehdipourparsa@gmail.com>
2022-10-13 17:22:59 +0400

    refactor : remove from property

--- CategoryService/Models/Category.cs
+++ CategoryService/Models/Category.cs
@@ -6,7 +6,6 @@ public class Category
 {
     public int Id { get; private set; }
     public string Name { get; private set; }
-    public string From { get; private set; }
     public DateTime CreateDate { get; private set; }
 
     public Category(string name)
@@ -22,13 +21,5 @@ public class Category
             return;
 
         Name = name;
-    }
-
-    public void SetFrom(string from)
-    {
-        if(From == from)
-            return;
-
-        From = from;
     }
 }
